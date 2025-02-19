be6a416936d3363fa1958e1c54ca0fbf42bc3823
陳思愷 <mordecai0304@gmail.com>
2025-02-19 10:39:28 +0800

    feat: 完成訂單列表

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -109,6 +109,49 @@
         </div>
       </div>
       </div>
+      <!-- 訂單列表 -->
+      <div class="my-5 row justify-content-center">
+      <FormApp class="col-md-6"
+            @submit="createOrder">
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <FieldApp id="email" name="email" type="email" class="form-control"
+          placeholder="請輸入 Email"
+          :rules="{ email: true, required: true }"
+          v-model="form.user.email"></FieldApp>
+          <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
+        </div>
+        <div class="mb-3">
+          <label for="name" class="form-label">收件人姓名</label>
+          <FieldApp id="name" name="姓名" type="text" class="form-control"
+                   placeholder="請輸入姓名" rules="required"
+                   v-model="form.user.name"></FieldApp>
+          <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+        </div>
+        <div class="mb-3">
+          <label for="tel" class="form-label">收件人電話</label>
+          <FieldApp id="tel" name="電話" type="tel" class="form-control"
+                   placeholder="請輸入電話" rules="required"
+                   v-model="form.user.tel"></FieldApp>
+          <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+        </div>
+        <div class="mb-3">
+          <label for="address" class="form-label">收件人地址</label>
+          <FieldApp id="address" name="地址" type="text" class="form-control"
+                   placeholder="請輸入地址" rules="required"
+                   v-model="form.user.address"></FieldApp>
+          <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
+        </div>
+        <div class="mb-3">
+          <label for="message" class="form-label">留言</label>
+          <textarea name="" id="message" class="form-control" cols="30" rows="10"
+                    v-model="form.message"></textarea>
+        </div>
+        <div class="text-end">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </FormApp>
+    </div>
     </div>
   </template>
 <script>
@@ -119,6 +162,15 @@ export default {
       product: {},
       status: {
         loadingItem: '' // 對應產品 ID
+      },
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: ''
+        },
+        message: ''
       },
       cart: {},
       coupon_code: ''
@@ -196,6 +248,14 @@ export default {
         this.getCart()
         this.isLoading = false
       })
+    },
+    createOrder () {
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/order`
+      const order = this.form
+      this.$http.post(url, { data: order })
+        .then((res) => {
+          console.log(res)
+        })
     }
   },
   created () {
